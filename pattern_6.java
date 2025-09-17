44c9f56677843548541ea61edce40ceccf1bf9fb
Raje2433 <163415339+Raje2433@users.noreply.github.com>
2025-09-17 11:40:55 +0530

    Create pattern_6.java

---
+++ pattern_6.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int rows = scan.nextInt();
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=(rows-i+1);j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
